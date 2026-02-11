17f7b4bf1512e0664cae7233db3453e1bde0e7b2
nvl0906 <navalherimampionona@gmail.com>
2026-02-12 00:29:19 +0300

    create trigger

--- scripts/deploy_face_attendance_to_gcr.sh
+++ scripts/deploy_face_attendance_to_gcr.sh
@@ -8,10 +8,9 @@ echo "############# Deploying the Cloud Run service $SERVICE_NAME"
 
 gcloud run deploy "$SERVICE_NAME" \
   --image "$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$SERVICE_NAME:$IMAGE_TAG" \
-  --region="$LOCATION" \
+  --region "$LOCATION" \
+  --project "$PROJECT_ID" \
+  --platform "managed" \
   --allow-unauthenticated \
-  --set-env-vars PROJECT_ID="$PROJECT_ID" \
-  --set-env-vars SUPABASE_URL="$SUPABASE_URL" \
-  --set-env-vars SUPABASE_KEY="$SUPABASE_KEY" \
-  --set-env-vars JWT_SECRET="$JWT_SECRET" \
-  --set-env-vars JWT_ALGORITHM="$JWT_ALGORITHM" \
+  --set-secrets=SUPABASE_URL=SUPABASE_URL:latest,SUPABASE_KEY=SUPABASE_KEY:latest,JWT_SECRET=JWT_SECRET:latest,JWT_ALGORITHM=JWT_ALGORITHM:latest
+  
